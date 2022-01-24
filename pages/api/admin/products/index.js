24b0b3ecaf4d3e3c2e15332e6a566a963e1ac78f
abbasrahmanii <absrn26@gmail.com>
2022-01-24 14:37:30 +0330

    Create and Delete Product

---
+++ pages/api/admin/products/index.js
@@ -0,0 +1,36 @@
+import nc from "next-connect";
+import { isAdmin, isAuth } from "../../../../utils/auth";
+import Product from "../../../../models/Product";
+import db from "../../../../utils/db";
+
+const handler = nc();
+handler.use(isAuth, isAdmin);
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const products = await Product.find({});
+  await db.disconnect();
+  res.send(products);
+});
+
+handler.post(async (req, res) => {
+  await db.connect();
+  const newProduct = new Product({
+    id: "p" + Math.random(),
+    name: "sample name",
+    image: "/images/shirt1.jpg",
+    price: 0,
+    popular: false,
+    brand: "sample brand",
+    category: "sample category",
+    quantity: 0,
+    countInStock: 0,
+    freeDelivery: false,
+  });
+
+  const product = await newProduct.save();
+  await db.disconnect();
+  res.send({ message: "Product Created", product });
+});
+
+export default handler;
